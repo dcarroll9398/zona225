2344be6ae4e19053e3d3e90276f75c4604201f9c
dcarroll9398 <dcarroll9398@yahoo.com>
2022-02-27 22:43:17 -0600

    load products from backend

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers({
+  productList: productListReducer,
+});
+
+const composeEnahcer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnahcer(applyMiddleware(thunk))
+);
+
+export default store;
